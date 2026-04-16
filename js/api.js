881be7dde69738d5bdc942064ff2c37417f5e424
tambakhmp <tambakhmp@gmail.com>
2026-04-16 11:46:16 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 // api.js — Komunikasi ke Google Apps Script
 const CONFIG = {
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbxjzcWQLHZY8nH7uypLbDyPIyYDHj9GMzNUV-_4I6lroMr_DAxGH_07zP46LtnG3wMmAw/exec'
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbxryHzWwjPsoBCg1hJ-XtDlNxXLsrnevfVPLE95jWMTxxUU4cPpVLMb22LeNlCQnZnphg/exec'
 };
 
 async function callAPI(action, payload = {}) {
