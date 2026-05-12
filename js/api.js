c2a2c39bf67ee67578b4ff19b6322692637933b9
tambakhmp <tambakhmp@gmail.com>
2026-05-12 21:34:10 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 // api.js — Komunikasi ke Google Apps Script
 const CONFIG = {
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbwa4o1q_azrnKXdMX-UP-PggmHNypF76k7Tpf-B1z9aIi2fTh6V1E4dloqEHg2H20G3KA/exec'
+  GAS_URL: 'https://script.google.com/macros/s/AKfycby4QOLJlDdAeoxY50AqTZWlx5sO6KKddMqUdnjPpV3Qb8DmrNjXRIqZYjEQGXpHp-60Qw/exec'
 };
 
 // Daftar action yang TIDAK boleh auto-logout meskipun error mengandung "Sesi tidak valid"
